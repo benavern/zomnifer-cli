80033ce199d719bf7fc0ee2a1f63f3b2be03da67
Benjamin Caradeuc <bencrdc@gmail.com>
2018-10-05 00:38:47 +0200

    Fix chooseAction import

--- zomnifer.js
+++ zomnifer.js
@@ -1,10 +1,10 @@
 const clean = require('./lib/clean')
-const inquirer = require('./lib/inquirer')
+const chooseAction = require('./lib/chooseAction')
 const actions = require('./lib/actions')
 
 module.exports = async os => {
   clean('Action')
-  const { action } = await inquirer.chooseAction()
+  const { action } = await chooseAction()
 
   clean(action)
   actions[action].run()
